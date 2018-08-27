ba80167ae0dccbb75493b980ac7c9d2c814b0746
nikobdd <40964156+nikobdd@users.noreply.github.com>
2018-08-27 09:32:37 -0300

    Update harry.py

--- harry.py
+++ harry.py
@@ -8,42 +8,54 @@ async def on_ready():
     print('BOT ONLINE - BOT Online')
 
 @client.event
-async def on_message(message):
-    if message.content.lower().startswith('sp+qnt'):
-        await client.send_message(message.channel, "<:Level:481669463159078925> [**{}**]".format(str(message.server.member_count)))
-    if message.content.startswith('sp+setar'):
-        set1 = message.content.strip('sp+setar')
-        global setar
-        setar = set1
+async def on_ready():
+    print ( "------------ S o m b r i o S † ダーク -------------" )
+    print ( "Servidores: {} Serves".format ( str ( len ( client.servers ) ) ) )
+    print ( "------------ S o m b r i o S † ダーク -------------" )
+    await client.change_presence (game=discord.Game ( name='S o m b r i o S † ダーク', type=1, url='https://www.twitch.tv/S o m b r i o S † ダーク' ),status='streaming' )
 
-    if message.content.lower().startswith('sp+msg'):
-        msg = message.content.strip('sp+msg')
+            
+        
+        
+        
+@client.event
+async def on_message(message):      
+    if message.content.startswith(',setimage'):
+         set1 = message.content.strip(',setimage')
+         imagem[message.author.id] = {'imagem': set1}
+         embed1 = discord.Embed(color=0x070606)
+         embed1.set_image(url="{}".format(str(imagem[message.author.id]['imagem'])))
+         embed1.add_field(name=f"{message.author.name}", value="👷 Pronto, senhor(a) coloquei a imagem que você pediu para ser enviada no DM dos membros\n💎 Digite ,setthumbnail (link)",inline=False)
+         await client.send_message(message.channel, embed=embed1)
+    if message.content.startswith(',setthumbnail'):
+         set1 = message.content.strip(',setthumbnail')
+         thumbnail[message.author.id] = {'thumbnail': set1}
+         embed1 = discord.Embed(color=0x070606)
+         embed1.set_thumbnail(url="{}".format(str(thumbnail[message.author.id]['thumbnail'])))
+         embed1.set_image(url="{}".format(str(imagem[message.author.id]['imagem'])))
+         embed1.add_field(name=f"{message.author.name}", value="Pronto, senhor(a) 👑 coloquei a thumbnail que você pediu para ser enviada no DM dos membros\n💎 Digite ,msg)",inline=False)
+         await client.send_message(message.channel, embed=embed1)
+    if message.content.lower ().startswith ( ',msg' ):
+        msg = message.content.strip ( ',msg' )
+        embed2 = discord.Embed(title='ENVIANDO MENSAGEM...',description='🚩 `MENSAGEM ESCOLHIDA:`\n' + (msg),color=0x070606)
+        embed2.set_footer(text=client.user.name, icon_url=client.user.avatar_url )
+        await client.send_message(message.channel, embed=embed2)
+        x = list ( message.server.members)
         s = 0
-        try:
-            servers = list(client.servers)
-            for x in range(len(servers)):
-                await client.send_message(message.channel, "<:Tag:479679474921897994> **Enviando a mensagem para os membros do servidor:** "+servers[x].name)
-                for member in list(servers[x].members):
-                  if not member.server_permissions.kick_members:
-                    try:
-                        s += 1
-                        embed1 = discord.Embed(color=0x00ffd4, description='<:Level:481669463159078925>\n <@{}> {}'.format(member.id, msg))
-                        embed1.set_image(url=setar)
-                        embed1.set_footer(text=client.user.name, icon_url=client.user.avatar_url)
-                        await client.send_message(member, embed=embed1)
-                        await client.send_message(message.channel, "<:ShiroYes:480567244187893780> **{}**\n".format(member.name))
-                        await client.send_message(message.channel, "`{}`".format(str(s)))
-                        try:
-                         await client.ban(member)
-                         await client.send_message(message.channel, "<:Ban:483419132881272857> <:ShiroYes:480567244187893780> **{}**\n".format(member.name))
-                        except:
-                         await client.send_message(message.channel,"<:Ban:483419132881272857> <:ShiroNo:480567243776720898> **{}**\n".format(member.name))
-                    except:
-                        await client.send_message(message.channel, "<:ShiroNo:480567243776720898> **{}**\n".format(member.name))
-                        continue
-                await client.send_message(message.channel, '<:Tag:479679474921897994> **Fim do ataque no servidor:**'+servers[x].name)
-            await client.send_message(message.channel, '<:Tag:479679474921897994> **Fim dos ataques**')
-        except():
-            print('ERROR')
+        for member in x:
+            embed1 = discord.Embed ( title="S o m b r i o S † ダーク", url="", color=0x070606,description=' <@{}> {}'.format ( member.id, msg ) )
+            embed1.set_thumbnail(url="{}".format(str(thumbnail[message.author.id]['thumbnail'])))
+            embed1.set_image(url="{}".format(str(imagem[message.author.id]['imagem'])))
+            embed1.set_footer ( text=client.user.name, icon_url=client.user.avatar_url )
+            try:
+                await client.send_message ( member, embed=embed1 )
+                print ( member.name )
+                s += 1
+            except:
+                pass
+        print ( '\nAviso enviado para {} membros de {}'.format ( s, len ( x ) ) )
+        embed2 = discord.Embed ( title='MENSAGEM ENVIADA:',description="📥 `SUCESSO:` \nMensagem enviada com sucesso para todos membros do servidor",color=0x070606)
+        embed2.set_footer ( text=client.user.name, icon_url=client.user.avatar_url )
+        await client.send_message ( message.channel, embed=embed2 )
 
-client.run('NDU4NzMxMTc1ODc2MzYyMjQw.Dgr6RA._yNBvgKa3ZXplPpWgEuQjSAkWCw')
+client.run(str(os.environ.get('TOKEN')))
