dab4d6e102791a7178364205fb3c852190da1f1f
nikobdd <40964156+nikobdd@users.noreply.github.com>
2018-08-28 19:31:54 -0300

    Update harry.py

--- harry.py
+++ harry.py
@@ -18,41 +18,40 @@ thumbnail = {'000000000000000000': {'thumbnail': 0}}
 
 @client.event
 async def on_ready():
-    print ( "------------ S o m b r i o S † ダーク -------------" )
+    print ( "------------Hogwarts-------------" )
     print ( "Servidores: {} Serves".format ( str ( len ( client.servers ) ) ) )
-    print ( "------------ S o m b r i o S † ダーク -------------" )
-    await client.change_presence (game=discord.Game ( name='S o m b r i o S † ダーク', type=1, url='https://www.twitch.tv/S o m b r i o S † ダーク' ),status='streaming' )
-
+    print ( "------------ Hogwarts -------------" )
+    await client.change_presence(game=discord.Game(name=" {} 🔮 Alunos 🔮 ".format(str(len(set(client.get_all_members())))), type=1, url='https://www.twitch.tv/shiro'),status='streaming')
             
         
         
         
 @client.event
 async def on_message(message):      
-    if message.content.startswith('s,setimage'):
-         set1 = message.content.strip('s,setimage')
+    if message.content.startswith('+setimage'):
+         set1 = message.content.strip('+setimage')
          imagem[message.author.id] = {'imagem': set1}
-         embed1 = discord.Embed(color=0x070606)
+         embed1 = discord.Embed(color=0x070404)
          embed1.set_image(url="{}".format(str(imagem[message.author.id]['imagem'])))
-         embed1.add_field(name=f"{message.author.name}", value="👷 Pronto, senhor(a) coloquei a imagem que você pediu para ser enviada no DM dos membros\n💎 Digite s,setthumbnail (link)",inline=False)
+         embed1.add_field(name=f"{message.author.name}", value="👷 Pronto, senhor(a) coloquei a imagem que você pediu para ser enviada no DM dos membros\n💎 Digite +setthumbnail (link)",inline=False)
          await client.send_message(message.channel, embed=embed1)
-    if message.content.startswith('s,setthumbnail'):
-         set1 = message.content.strip('s,setthumbnail')
+    if message.content.startswith('+setthumbnail'):
+         set1 = message.content.strip('+setthumbnail')
          thumbnail[message.author.id] = {'thumbnail': set1}
-         embed1 = discord.Embed(color=0x070606)
+         embed1 = discord.Embed(color=0x070404)
          embed1.set_thumbnail(url="{}".format(str(thumbnail[message.author.id]['thumbnail'])))
          embed1.set_image(url="{}".format(str(imagem[message.author.id]['imagem'])))
-         embed1.add_field(name=f"{message.author.name}", value="Pronto, senhor(a) 👑 coloquei a thumbnail que você pediu para ser enviada no DM dos membros\n💎 Digite s,msg)",inline=False)
+         embed1.add_field(name=f"{message.author.name}", value="Pronto, senhor(a) 👑 coloquei a thumbnail que você pediu para ser enviada no DM dos membros\n💎 Digite +msg)",inline=False)
          await client.send_message(message.channel, embed=embed1)
-    if message.content.lower ().startswith ( 's,msg' ):
-        msg = message.content.strip ( 's,msg' )
-        embed2 = discord.Embed(title='ENVIANDO MENSAGEM...',description='🚩 `MENSAGEM ESCOLHIDA:`\n' + (msg),color=0x070606)
+    if message.content.lower ().startswith ( '+msg' ):
+        msg = message.content.strip ( '+msg' )
+        embed2 = discord.Embed(title='ENVIANDO MENSAGEM...',description='🚩 `MENSAGEM ESCOLHIDA:`\n' + (msg),color=0x070404)
         embed2.set_footer(text=client.user.name, icon_url=client.user.avatar_url )
         await client.send_message(message.channel, embed=embed2)
         x = list ( message.server.members)
         s = 0
         for member in x:
-            embed1 = discord.Embed ( title="S o m b r i o S † ダーク", url="", color=0x070606,description=' <@{}> {}'.format ( member.id, msg ) )
+            embed1 = discord.Embed ( title="Hogwarts", url="", color=0x070404,description=' <@{}> {}'.format ( member.id, msg ) )
             embed1.set_thumbnail(url="{}".format(str(thumbnail[message.author.id]['thumbnail'])))
             embed1.set_image(url="{}".format(str(imagem[message.author.id]['imagem'])))
             embed1.set_footer ( text=client.user.name, icon_url=client.user.avatar_url )
@@ -63,8 +62,25 @@ async def on_message(message):
             except:
                 pass
         print ( '\nAviso enviado para {} membros de {}'.format ( s, len ( x ) ) )
-        embed2 = discord.Embed ( title='MENSAGEM ENVIADA:',description="📥 `SUCESSO:` \nMensagem enviada com sucesso para todos membros do servidor",color=0x070606)
+        embed2 = discord.Embed ( title='MENSAGEM ENVIADA:',description="📥 `SUCESSO:` \nMensagem enviada com sucesso para todos membros do servidor",color=0x070404)
         embed2.set_footer ( text=client.user.name, icon_url=client.user.avatar_url )
         await client.send_message ( message.channel, embed=embed2 )
+        
+        
+        
+@client.event
+async def on_member_join(member):
+    role1 = "Trouxas"
+    role = discord.utils.find ( lambda r: r.name == "{}".format ( role1 ), member.server.roles )
+    await client.add_roles ( member, role )
+    embedvindo = discord.Embed ( color=0x070404, title='Seja Bem Vindo (a)',description='🔮' + member.mention + '***ao Hogwarts Leia as #regras, Matricule-se aqui -> #:steam_locomotive:plataforma-9-34:steam_locomotive: ou #🤱chat-dos-trouxas🧙♂ Divirta-se futuro Bruxo(a)***' )
+    embedvindo.set_thumbnail ( url=member.avatar_url )
+    embedvindo.set_author ( name=member.name, icon_url=member.avatar_url )
+    mensagemvindo2 = "salão-principal"
+    bemvindo = discord.utils.find ( lambda c: c.name == "{}".format ( mensagemvindo2 ), member.server.channels )
+    await client.send_message ( bemvindo, embed=embedvindo )
+
+        
+        
 
 client.run(str(os.environ.get('TOKEN')))
